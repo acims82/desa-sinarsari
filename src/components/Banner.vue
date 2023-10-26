396736cfebb8abee8c8dd095bfecc01619945dd1
Acim <acims82@gmail.com>
2023-10-26 18:48:17 +0700

    perubahan ukuran logo pada banner

--- src/components/Banner.vue
+++ src/components/Banner.vue
@@ -2,7 +2,7 @@
   <div class="bg-green-600 h-[300px] flex">
     <img
       src="@/assets/logo-bogor.png"
-      class="w-25 h-10 opacity-60 m-10"
+      class="w-20 h-10 opacity-60 m-10"
       alt=""
     />
     <div class="ml-10 flex flex-col justify-center">
