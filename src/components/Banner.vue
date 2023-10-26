a5af728196fd7ad9059e2c7a2532531141e670c3
Acim <acims82@gmail.com>
2023-10-26 18:06:08 +0700

    perubahan ukuran huruf pada banner

--- src/components/Banner.vue
+++ src/components/Banner.vue
@@ -2,13 +2,13 @@
   <div class="bg-green-600 h-[300px] flex">
     <img
       src="@/assets/logo-bogor.png"
-      class="w-15 h-20 opacity-60 m-20"
+      class="w-15 h-20 opacity-40 m-10"
       alt=""
     />
     <div class="ml-10 flex flex-col justify-center">
       <p class="text-white text-2xl mb-2">Profile Desa</p>
       <!-- Sesuaikan nama desa masing-masing -->
-      <p class="text-lg leading-none uppercase text-white font-bold mb-2">Desa Sinarsari</p>
+      <p class="text-lg leading-none uppercase text-white font-bold mb-3">Desa Sinarsari</p>
       <p class="text-white text-2xl">Kecamatan Dramaga Kab. Bogor</p>
     </div>
     <div class="grid place-items-center ml-auto mr-20">
