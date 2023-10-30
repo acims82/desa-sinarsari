f021223b36371b0a22bfbf65f7b2f46d7eec464f
acims82 <144505808+acims82@users.noreply.github.com>
2023-10-31 00:14:41 +0700

    Update penghapusan gambar visi misi desa

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -12,10 +12,6 @@ import Sidebar from "../components/Sidebar.vue";
     <Sidebar />
 
     <div class="flex-1 p-10">
-      <p class="text-3xl font-semibold my-4">Struktur Desa</p>
-      <img src="@/assets/strukturdesa.jpg" alt="" />
-      </>
-        
       <p class="text-3xl font-semibold">Visi & Misi Desa</p>
       <p class="text-lg font-semibold">Visi</p>
       <p>
