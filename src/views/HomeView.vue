ed4039f430b7a7e8510fa7599477d0e48a5eecfe
acims82 <144505808+acims82@users.noreply.github.com>
2023-11-02 23:42:31 +0700

    Update HomeView.vue

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -13,8 +13,8 @@ import Sidebar from "../components/Sidebar.vue";
 
     <div class="flex-1 p-10">
       <p class="text-3xl font-semibold">GAMBARAN UMUM DESA SINARSARI</p>
-
       <img src="@/assets/visimisi-desa.jpg" alt="" />
+      <p class="text-3xl font-semibold">LETAK GEOGRAFIS</p>
       <img src="@/assets/peta-desa-2022.jpg" alt="" />
     </div>
   </main>
