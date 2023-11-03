78550a24a1ccd0b3fddd35daaf4a2077279bd90f
acims82 <144505808+acims82@users.noreply.github.com>
2023-11-03 23:52:15 +0700

    Update Banner

--- src/components/Banner.vue
+++ src/components/Banner.vue
@@ -1,3 +1,24 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class LabelListener implements ActionListener
+{
+	JLabel lbl;
+	//Konstruktor
+	public LabelListener(JLabel lblParam)
+	{
+		lbl = lblParam;
+	} //Akhir Konstruktor
+	
+	public void actionPerformed(ActionEvent act)
+	{
+		String text = lbl.getText();
+		String text2 = text.substring(1) + text.substring(0,1);
+		lbl.setText(text2);
+	}
+}
+
 <template>
   <div class="bg-teal-700 h-[300px] flex">
     <img 
