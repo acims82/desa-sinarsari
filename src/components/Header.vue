f8243b338fdb11555233c0101478d145ab9478ce
acims82 <144505808+acims82@users.noreply.github.com>
2023-11-04 00:41:31 +0700

    Update Header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -15,7 +15,7 @@
       </p>
     </div>
 
-    <div class="ml-10 pr-100 flex flex-col justify-center">
+    <div class="h-full ml-10 pr-100 flex flex-col justify-center">
       <marquee direction='left' class="text-white"> Selamat Datang di Website Desa Sinarsari</marquee>
     </div>	
     
