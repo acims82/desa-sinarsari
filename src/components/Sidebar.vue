54a9f50483eadda5734826e7a127b35e9dca2aa7
acims82 <144505808+acims82@users.noreply.github.com>
2023-11-03 23:29:19 +0700

    Update Sidebar.vue

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -16,12 +16,7 @@
     >
       <router-link to="/about">Tentang Desa</router-link>
     </div>
-
-    <div
-      class="text-lg py-4 px-1 border-b border-slate-200 text-[#00074e] font-semibold cursor-pointer"
-    >
-      <router-link to="/medsos">Medsos Desa</router-link>
-    </div>
+    
   </div>
 </template>
 
